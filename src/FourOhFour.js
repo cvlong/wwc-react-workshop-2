59e5ff8a92d1701d7346166c448a0640286d67eb
Christina <cvlong@gmail.com>
2016-09-01 20:49:54 -0700

    Add timeout to redirect

--- src/FourOhFour.js
+++ src/FourOhFour.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 
 export default class FourOhFour extends Component {
+  // Can add AJAX request to save incorrect url so you know if links are broken
+  componentDidMount() {
+    setTimeout(() => {
+        this.props.history.push('/')
+    }, 1000);
+  }
   render() {
     return (
       <div>
