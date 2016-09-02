688658a67f8c9b60615bfc403577c3bef692183e
Christina <cvlong@gmail.com>
2016-09-01 20:21:26 -0700

    Add 404 page

---
+++ src/FourOhFour.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class FourOhFour extends Component {
+  render() {
+    return (
+      <div>
+        Page cannot be found<br />
+        <img src='http://www.fillmurray.com/200/200'/>
+      </div>
+    );
+  }
+}
